9963ae44cd7ad9d42b1bc97204353c0c13fb51f7
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-02-07 23:18:25 +0100

    Add lefthook for pre-commit checks

--- shell.nix
+++ shell.nix
@@ -2,5 +2,5 @@ with import <nixpkgs> { };
 
 pkgs.mkShell {
   name = "sondr3-nix";
-  buildInputs = with pkgs; [ haskellPackages.niv hugo ];
+  buildInputs = with pkgs; [ haskellPackages.niv hugo lefthook ];
 }
