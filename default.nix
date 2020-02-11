303777c82808fe1593ca169cb4ad97af651a45a5
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-02-11 22:27:21 +0100

    Remove rust-analyzer, isn't worth the trouble

--- default.nix
+++ default.nix
@@ -16,6 +16,5 @@
 
   maple = pkgs.callPackage ./pkgs/maple { };
   ripcord = pkgs.callPackage ./pkgs/ripcord { };
-  rust-analyzer = pkgs.callPackage ./pkgs/rust-analyzer { };
   xmobar-wrapped = pkgs.callPackage ./pkgs/xmobar-wrapped { };
 }
